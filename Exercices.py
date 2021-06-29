31491f96abd607ebb1097b8d449ff2e00c11bbf8
julienPalleau <jpalleau@gmail.com>
2021-06-29 11:39:26 +0200

    exercises
    initial commit

---
+++ Exercices.py
@@ -0,0 +1,16 @@
+# 17 - args-kwargs
+
+# def exponentiate(base, exponent):
+# 	return base ** exponent
+#
+# print(exponentiate(2, 4))
+#
+#
+# exp = lambda base, exponent: base ** exponent
+# print(exp(2, 5))
+# ----------------------------------
+# def testFunc(*args):
+#     print(sum(args))
+#
+#
+# testFunc(2, 4)
