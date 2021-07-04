b1a3f63db5e1c8aa451467ca2f6f33cb2bff0f31
julienPalleau <jpalleau@gmail.com>
2021-07-04 10:03:00 +0200

    Functions

--- Exercices.py
+++ Exercices.py
@@ -439,7 +439,87 @@
 ###################
 # Day12 Functions #
 ###################
+# # Exercice 1 Define four functions: add, subtract, divide, and multiply. Each function should take two arguments, and
+# # they should print the result of the arithmetic operation indicated by the function name.
+# # When orders matters for an operation, the first argument should be treated as the left operand, and the second
+# # argument should be treated as the right operand. For example, if the user passes in 6 and 2 to subtract, the result
+# # should be 4, not -4.
+# # You should also make sure that the user can’t pass in 0 as the second argument for divide. If the user provides 0, you
+# # should print a warning instead of calculating their division.
+# def add(terme1, terme2):
+#     print(f"{terme1 + terme2}")
+#
+#
+# def substract(terme1, terme2):
+#     print(f"{terme1 - terme2}")
+#
+#
+# def divide(terme1, terme2):
+#     print(f"{terme1 / terme2}")
+#
+#
+# def multiply(terme1, terme2):
+#     print(f"{terme1 * terme2}")
+#
+#
+# add(5, 7)
+# substract(terme1=10, terme2=5)
+# divide(terme1=20, terme2=4)
+# multiply(5, 4)
 
+# # Exercice2 Define a function called print_show_info that has a single parameter. The argument passed to it will be a
+# # dictionary with some information about a T.V. show. For example:
+#
+tv_show = {
+    "title": "Breaking Bad",
+    "seasons": 5,
+    "initial_release": 2008
+}
+
+
+#  print_show_info(tv_show)
+# The print_show_info function should print the information stored in the dictionary, in a nice way. For example:
+#  Breaking Bad (2008) - 5 seasons
+# Remember you must define your function before calling it!
+
+
+def print_show_tv(tv_show):
+    print(f"{tv_show.get('title', 'unknown')} ({tv_show.get('initial_release', 'unknow')}) - "
+          f"{tv_show.get('seasons', 'unknown')} seasons")
+
+
+#
+#
+# print_show_tv(tv_show)
+#
+# # Exercice 3 Below you’ll find a list containing details about multiple TV series.
+# #
+series = [
+    {"title": "Breaking Bad", "seasons": 5, "initial_release": 2008},
+    {"title": "Fargo", "seasons": 4, "initial_release": 2014},
+    {"title": "Firefly", "seasons": 1, "initial_release": 2002},
+    {"title": "Rick and Morty", "seasons": 4, "initial_release": 2013},
+    {"title": "True Detective", "seasons": 3, "initial_release": 2014},
+    {"title": "Westworld", "seasons": 3, "initial_release": 2016},
+]
+# # Use your function, print_show_info, and a for loop, to iterate over the series list, and call your function once for
+# # each iteration, passing in each dictionary. You should end up with each series printed in the appropriate format.
+for line in series:
+    print_show_tv(line)
+
+# Exercice 4 Create a function to test if a word is a palindrome. A palindrome is a string of characters that are
+# identical whether read forwards or backwards. For example, “was it a car or a cat I saw” is a palindrome.
+word = "was it a car or a cat I saw"
+
+
+def is_apalindrome(word):
+    if word[::-1].lower().replace(" ", "") == word.lower().replace(" ", ""):
+        print("Il s'agit bien d'un palindrome")
+    else:
+        print("Desole ceci n'est pas un palindrome")
+
+
+is_apalindrome(word)
 
 
 #########
